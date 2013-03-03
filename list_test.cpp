761b9d19d9504f11320619ec51e2e41ffcf8271c
DL8 <dl8.nir@gmail.com>
2013-03-04 00:06:47 +0200

    modified list_test.cpp for copying and insertion test

--- list_test.cpp
+++ list_test.cpp
@@ -6,14 +6,34 @@ using namespace DataStructures;
 
 int main() {
 	List<int> n;
-	List<double> n2;
 	auto ii = n.begin();
-	n.insert (ii , 3);
-	n.insert (ii, 4);
-	n.insert (ii, 6);
-	for (auto &j:n) {
+	n.insert (ii , 1);
+	n.insert (ii, 2);
+	n.insert (ii, 3);
+	n.insert (ii - 1, 4);
+for (auto & j: n) {
 		cout << j << " ";
 	}
 	cout << endl;
+
+	List<int> n2 (n);
+	n2.insert (n2.begin() + 1, 5);
+for (auto & j: n2) {
+		cout << j << " ";
+	}
+	cout << endl;
+
+	n2 = n;
+	n2.insert(n2.end()-2, 6);
+for (auto & j: n2) {
+		cout << j << " ";
+	}
+	cout << endl;
+	
+	for (auto & j: n) {
+		cout << j << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
