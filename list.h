88feceafc532993f1d7ab31ee1176eaf5d8132ff
DL8 <dl8.nir@gmail.com>
2013-03-03 11:44:43 +0200

    iterator arithmetics, improved decrement, documented class

--- list.h
+++ list.h
@@ -1,6 +1,9 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+#include <cstdlib>
+using std::abs;
+
 namespace DataStructures {
 
 	template<class T> class List {
@@ -33,41 +36,118 @@ namespace DataStructures {
 	public:
 		class Iterator {
 			Node *current;
+			Node *last;
 		public:
-			Iterator (Node *position) : current (position) {}
+			/**
+			 * @brief creates a new iterator with the given position
+			 * nullptr is treated as one after the last element
+			 * @param position the initial position
+			 */
+			Iterator (Node *position) : current (position) {
+				Node *last = head;
+				while ( (last != nullptr) && (last->next != nullptr)) {
+					last = last->next;
+				}
+			}
 
+			/**
+			 * @brief suffix increment
+			 * @return *this
+			 */
 			Iterator &operator++() {
 				current = current->next;
 				return *this;
 			}
 
+			/**
+			 * @brief prefix increment
+			 * @return *this
+			 */
 			Iterator &operator++ (int) {
 				return operator++();
 			}
 
+			/**
+			 * @brief suffix decrement
+			 * @return *this
+			 */
 			Iterator &operator--() {
-				current = current->prev;
+				if (current == nullptr) {
+					current = last;
+				} else {
+					current = current->prev;
+				}
 				return *this;
 			}
 
+			/**
+			 * @brief prefix decrement
+			 * @return *this
+			 */
 			Iterator &operator-- (int) {
 				return operator--();
 			}
 
+			/**
+			 * @brief iterator dereference
+			 * @return a reference to the value at the iterator's position
+			 */
 			T &operator*() {
 				return current->value;
 			}
 
+			/**
+			 * @brief iterator const dereference
+			 * @return a const reference to the value at the iterator's position
+			 */
 			const T &operator*() const {
 				return current->value;
 			}
 
+			/**
+			 * @brief iterator comparison
+			 * @param other the iterator to compare with
+			 * @return true if both iterators point to the same element of the same list
+			 */
 			bool operator== (const Iterator &other) const {
 				return this->current == other.current;
 			}
 
+			/**
+			 * @brief iterator comparison
+			 * @param other the iterator to compare with
+			 * @return false if both iterators point to the same element of the same list
+			 */
 			bool operator!= (const Iterator &other) const {
 				return ! (*this == other);
+			}
+
+			/**
+			 * @brief iterator +=
+			 * @param offset offset to shift the current iterator
+			 * @return *this
+			 */
+			Iterator &operator+= (int offset) {
+				bool add = (offset > 0);
+				offset = abs (offset);
+				if ( (offset > 0) && (current == nullptr)) {
+					current = last;
+					offset--;
+				}
+				while (offset > 0) {
+					current = add ? (current->next) : (current->prev);
+					offset--;
+				}
+				return *this;
+			}
+
+			/**
+			 * @brief iterator -=
+			 * @param offset offset to shift the current iterator
+			 * @return *this
+			 */
+			Iterator &operator-= (int offset) {
+				return *this += -offset;
 			}
 		};
 	private:
@@ -104,7 +184,34 @@ namespace DataStructures {
 		Iterator end() const {
 			return Iterator (nullptr);
 		}
+
+		/** TODO: make this safer
+		 * @brief inserts a new item before the given iterator
+		 * @param before an iterator before which the item will be inserted
+		 * @return *this
+		 */
+		List &insert (const Iterator &before, const T &data) {
+			Node *newNode = nodeCreate (data);
+			return *this;
+		}
 	};
+
+	template<class T>
+	typename List<T>::Iterator operator+ (const typename List<T>::Iterator &iter, const int offset) {
+		typename List<T>::Iterator ret = iter;
+		ret += offset;
+		return ret;
+	}
+
+	template<class T>
+	typename List<T>::Iterator operator+ (const int offset, const typename List<T>::Iterator &iter) {
+		return iter + offset;
+	}
+
+	template<class T>
+	typename List<T>::Iterator operator- (const typename List<T>::Iterator &iter, const int offset) {
+		return iter + (-offset);
+	}
 }
 
 #endif
